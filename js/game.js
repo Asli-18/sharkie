650fa0171c179bf9f13a693c90d7b455036b2829
Aslihan Erdogan <asli.erdogan.gazel@gmail.com>
2025-07-30 15:25:24 +0200

    refactor: outsorced Image and add World

--- js/game.js
+++ js/game.js
@@ -1,6 +1,19 @@
 let canvas;
-let character = new Image();
-function init(){
+let ctx;
+
+let world = new World();
+
+function init() {
     canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+
+    console.log("My Character is ", world.sharkie);
+    
+    // character.src = '../img/1.Sharkie/3.Swim/1.png';
+
+
+    // ctx.drawImage(character, 20, 20, 150, 130);
+
 
 }
