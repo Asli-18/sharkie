b1bf35d13668e5262478053448950d028976297f
Aslihan Erdogan <asli.erdogan.gazel@gmail.com>
2025-08-27 09:18:01 +0200

    refactor: add new img and functions

--- models/counter/health-counter.class.js
+++ models/counter/health-counter.class.js
@@ -1,11 +1,40 @@
-class HealthCounter extends MovableObject{
-    width = 45;
-    height = 45;
+class HealthCounter extends DrawableObject {
+    IMAGES = [
+        'assets/img/resource-display/progressbar-life-green-0.png',
+        'assets/img/resource-display/progressbar-life-green-20.png',
+        'assets/img/resource-display/progressbar-life-green-40.png',
+        'assets/img/resource-display/progressbar-life-green-60.png',
+        'assets/img/resource-display/progressbar-life-green-80.png',
+        'assets/img/resource-display/progressbar-life-green-100.png'
+    ];
+
+    percentage = 100;
+
+    constructor() {
+        super().loadImages(this.IMAGES);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
 
 
-    constructor(x, y) {
-        super().loadImage('assets/img/resource-display/life-100.png');
-        this.x = x;
-        this.y = y;
+    }
+
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
