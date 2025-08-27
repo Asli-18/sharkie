a44a64b5a88166eea72de3b517c721c3b8dc4407
Aslihan Erdogan <asli.erdogan.gazel@gmail.com>
2025-08-27 09:16:09 +0200

    refactor: outsourced div. func and add isHurt func

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,16 +1,10 @@
-class MovableObject {
-    x = 10;
-    y = 200;
-    img;
-    width = 200;
-    height = 180;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection;
     energy = 100;
     speedY = 0;
     acceleration = 2.5;
+    lastHit = 0;
     // constructor(x, y) {
     //     this.x = x;
     //     this.y = y;
@@ -29,26 +23,6 @@ class MovableObject {
         return this.y < 700;
     }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-
-    drawFrame(ctx) {
-        if (this instanceof Sharkie || this instanceof JellyFish || this instanceof PufferFish || this instanceof Coin || this instanceof Whale) {
-            ctx.beginPath();
-            ctx.lineWidth = '2';
-            ctx.strokeStyle = 'purple';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
-
     isColliding(movebleObject) {
         return this.x + this.width > movebleObject.x &&
             this.y + this.height > movebleObject.y &&
@@ -60,19 +34,19 @@ class MovableObject {
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
         }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 0.5;
     }
 
     isDead() {
         return this.energy == 0;
-    }
-
-    loadImages(array) {
-        array.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     animate() {
