3de9611bceb1cdee9f4b599b97d00f911da9d844
Aslihan Erdogan <asli.erdogan.gazel@gmail.com>
2025-09-11 10:16:18 +0200

    refactor: code structure improved

--- models/enemies/jelly-fish.class.js
+++ models/enemies/jelly-fish.class.js
@@ -1,56 +1,78 @@
 class JellyFish extends MovableObject {
     width = 50;
     height = 50;
-    IMAGES_SWIMMING = [
-        'assets/img/enemy/jelly-fish-yellow-1.png',
-        'assets/img/enemy/jelly-fish-yellow-2.png',
-        'assets/img/enemy/jelly-fish-yellow-3.png',
-        'assets/img/enemy/jelly-fish-yellow-4.png',
-        'assets/img/enemy/jelly-fish-green-1.png',
-        'assets/img/enemy/jelly-fish-green-2.png',
-        'assets/img/enemy/jelly-fish-green-3.png',
-        'assets/img/enemy/jelly-fish-green-4.png'
-    ];
-
-    // IMAGES_PUFFER_FISH_YELLOW = [
-    //     'assets/img/enemy/jelly-fish-yellow-1.png',
-    //     'assets/img/enemy/jelly-fish-yellow-2.png',
-    //     'assets/img/enemy/jelly-fish-yellow-3.png',
-    //     'assets/img/enemy/jelly-fish-yellow-4.png'
-    // ]; 
-
-    // IMAGES_PUFFER_FISH_GREEN = [
-    //     'assets/img/enemy/jelly-fish-green-1.png',
-    //     'assets/img/enemy/jelly-fish-green-2.png',
-    //     'assets/img/enemy/jelly-fish-green-3.png',
-    //     'assets/img/enemy/jelly-fish-green-4.png'
-    // ];
-
     speed = 0.05;
     direction = 1;
-    // 'assets/img/enemy/jelly-fish-lila-1.png'
-    constructor(imagePath = 'assets/img/enemy/jelly-fish-yellow-1.png') {
-        super().loadImage(imagePath);
-        this.loadImages(this.IMAGES_SWIMMING);
-        this.x = 250 + Math.random() * 2500;
-        this.y = 30 + Math.random() * 400;
-        this.speed = 0.05 + Math.random() * 0.1;
+    VARIANTS = {
+        yellowGreen: [
+            'assets/img/enemy/jelly-fish-yellow-1.png',
+            'assets/img/enemy/jelly-fish-yellow-2.png',
+            'assets/img/enemy/jelly-fish-yellow-3.png',
+            'assets/img/enemy/jelly-fish-yellow-4.png',
+            'assets/img/enemy/jelly-fish-green-1.png',
+            'assets/img/enemy/jelly-fish-green-2.png',
+            'assets/img/enemy/jelly-fish-green-3.png',
+            'assets/img/enemy/jelly-fish-green-4.png'
+        ],
+        lilaPink: [
+            'assets/img/enemy/jelly-fish-lila-1.png',
+            'assets/img/enemy/jelly-fish-lila-2.png',
+            'assets/img/enemy/jelly-fish-lila-3.png',
+            'assets/img/enemy/jelly-fish-lila-4.png',
+            'assets/img/enemy/jelly-fish-pink-1.png',
+            'assets/img/enemy/jelly-fish-pink-2.png',
+            'assets/img/enemy/jelly-fish-pink-3.png',
+            'assets/img/enemy/jelly-fish-pink-4.png'
+        ]
+    }
+
+    constructor(variant = "yellowGreen") {
+        super();
+        this.initJellyFish(variant);
+    }
+
+    initJellyFish(variant) {
+        this.setVariant(variant);
+        this.loadImages(this.images);
+        this.setRandomPosition();
+        this.setRandomSpeed();
         this.animate();
     }
 
-    animate() {
-        setInterval(() => {
-            let i = this.currentImage % this.IMAGES_SWIMMING.length;
-            let path = this.IMAGES_SWIMMING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+    setVariant(variant) {
+        // this.images = this.VARIANTS[variant] || this.VARIANTS.yellowGreen;
+        if (this.VARIANTS[variant]) {
+            this.images = this.VARIANTS[variant];
+        } else {
+            this.images = this.VARIANTS.yellowGreen;
+        }
+    }
 
-        }, 250);
-        setInterval(() => {
-            this.y += this.speed * this.direction;
-            if (this.y > 400 || this.y < 50) {
-                this.direction *= -1;
-            }
-        }, 1000 / 60);
+    setRandomPosition() {
+        this.x = 250 + Math.random() * 2500;
+        this.y = 30 + Math.random() * 400;
+    }
+
+    setRandomSpeed() {
+        this.speed = 0.05 + Math.random() * 0.1;
+    }
+
+    animate() {
+        setInterval(() => this.animationJellyFish(), 250);
+        setInterval(() => this.moveJellyFish(), 1000 / 60);
+    }
+
+    animationJellyFish() {
+        let i = this.currentImage % this.images.length;
+        let path = this.images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+    moveJellyFish() {
+        this.y += this.speed * this.direction;
+        if (this.y > 400 || this.y < 50) {
+            this.direction *= -1;
+        }
     }
 }
