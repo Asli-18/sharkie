9b34c6b26b668b9512484f2163cf24d3c4dbc77d
Aslihan Erdogan <asli.erdogan.gazel@gmail.com>
2025-11-03 09:27:42 +0100

    feat: add mobile button function

--- models/core/keyboard.class.js
+++ models/core/keyboard.class.js
@@ -1,4 +1,4 @@
-class Keyboard{
+class Keyboard {
     LEFT = false;
     RIGHT = false;
     UP = false;
@@ -11,4 +11,68 @@ class Keyboard{
     // key limit to fire only one bubble
     canShootW = true;
     canShootE = true;
+
+    constructor() {
+        this.bindBtnPressEvents();
+    }
+
+    bindBtnPressEvents() {
+        document.getElementById('btn-left').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.LEFT = true;
+        });
+        document.getElementById('btn-left').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.LEFT = false;
+        });
+        document.getElementById('btn-right').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.RIGHT = true;
+        });
+        document.getElementById('btn-right').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.RIGHT = false;
+        });
+        document.getElementById('btn-down').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.DOWN = true;
+        });
+        document.getElementById('btn-down').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.DOWN = false;
+        });
+        document.getElementById('btn-up').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.UP = true;
+        });
+        document.getElementById('btn-up').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.UP = false;
+        });
+
+        document.getElementById('btn-air-bubble').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.W = true;
+        });
+        document.getElementById('btn-air-bubble').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.W = false;
+        });
+        document.getElementById('btn-poison-bubble').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.E = true;
+        });
+        document.getElementById('btn-poison-bubble').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.E = false;
+        });
+        document.getElementById('btn-fin-slap').addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.F = true;
+        });
+        document.getElementById('btn-fin-slap').addEventListener('touchend', (event) => {
+            event.preventDefault();
+            this.F = false;
+        });
+    }
 }
